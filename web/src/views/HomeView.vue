ede46ad92cba5359077f61f88d26f95ebba8be1e
sml_wang <2818018141@qq.com>
2024-01-23 19:48:25 +0800

    update: add 2024 contest year

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -107,6 +107,9 @@ onMounted(() => {
 		<n-p>
 			<n-ul>
 				<n-li>
+					<n-a @click="router.push('/list/2024-all')">2024 赛季</n-a>。
+				</n-li>
+				<n-li>
 					<n-a @click="router.push('/list/2023-all')">2023 赛季</n-a>。
 				</n-li>
 				<n-li>
